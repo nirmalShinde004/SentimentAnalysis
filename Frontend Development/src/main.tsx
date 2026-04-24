ef6477d0e19fea25f098063dafce426047a88f48
NihalShinde4933 <nihal4933shinde@gmail.com>
2026-04-25 01:10:43 +0530

    Changed app.py

--- Frontend Development/src/main.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./app/routes";
-import "./styles/index.css";
-
-createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={router} />
-);
